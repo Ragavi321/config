1623ad554e08afe2765014656430ddc540e74075
Ragavi321 <70432566+Ragavi321@users.noreply.github.com>
2022-06-30 17:08:26 +0530

    Add files via upload

---
+++ readconfig.py
@@ -0,0 +1,13 @@
+import os
+
+def read_config_file():
+    env = os.environ['Environment']
+    project = os.environ['Project']
+    json_config_path = os.path.join(sys.path[0], project + "/" + "config_" + env + ".json")
+    return json_config_path
+
+def load_config_file():
+    config_file_name = read_config_file()
+    file = open(config_file_name)
+    config_data = json.load(file)
+    return config_data
